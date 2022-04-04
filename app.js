61831d7b7ae129f570cc8f0f6f72e291bd710412
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-04-04 03:27:42 +0200

    adding the first router to get all books

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
+const express=require('express')
+const app=express()
 
+const posts=[
+    {
+        username:'abdo ali 1',
+        post:"post one"
+    },
+    {
+        username:'abdo ali 2',
+        post:"post two"
+    },{
+        username:'abdo ali 3',
+        post:"post three"
+    }
+]
+
+app.get('/posts',(req,res)=>{
+    res.json(posts)
+})
+
+app.listen(3000,console.log('server is listening to port 3000'))
 
