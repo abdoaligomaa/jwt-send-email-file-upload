d378f8f22e410e30e33f77165598457b81f7fe79
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-04-05 05:16:10 +0200

    add

--- app.js
+++ app.js
@@ -1,41 +1,21 @@
 const express=require('express')
 const app=express()
 
-const multer=require('multer')
+
+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
 
 
 app.use(express.json())
 
-const storageOne=multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,'./singleImages')
+const
 
-    },
-    filename:(req,file,cb)=>{
-        cb(null,file.originalname)
-    }
-})
-const storageMult=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'./multImages')
 
-    },
-    filename:(req,file,cb)=>{
-        cb(null,Date.now()+file.originalname)
-    }
-})
 
-const uploadOne=multer({storage:storageOne})
-const uploadMult=multer({storage:storageMult})
-const upload=multer({dest:"./images"})
-
-app.post('/singlFiles',uploadOne.single('image'),(req,res)=>{
-   console.log(req.file)
+app.post('/singlFiles',(req,res)=>{
    res.send('true')
 })
 
-app.post('/multFiles',uploadMult.fields([{name:"image1", maxCount: 1},{name:"image2", maxCount: 1}]),(req,res)=>{
-    console.log(req.files)
+app.post('/multFiles',(req,res)=>{
     res.send('true')
 
 })
