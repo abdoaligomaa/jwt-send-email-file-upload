b47c62cb3c4e2c418559b6bb4f3e8bec1c8c4378
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-04-08 02:20:02 +0200

    adding middleware to the route

--- middleware.js
+++ middleware.js
@@ -1,10 +1,17 @@
 const express=require('express')
 const app=express()
 
-
+const middleware=(req,res,next)=>{
+    const a =true
+    if (a===true) {
+        console.log('you can not enter to the next')
+    } else {
+        next()        
+    }
+}
 
 app.use(express.json())
-app.get('/',(req,res)=>{
+app.get('/',middleware,(req,res)=>{
     res.send('done')
 })
 
